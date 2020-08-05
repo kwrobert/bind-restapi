02a83e4332119b66ced9c92e3ddda8e98131787f
Kyle Robertson <kyle.wesley@me.com>
2020-08-05 11:59:19 -0400

    Updates to install script

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 DOMAIN=mathworks.com
 
-dnssec-keygen -a HMAC-SHA512 -b 512 -n USER $DOMAIN
+yum install -y python3-pip bind-utils
+
 cp ./bind-api.service /etc/systemd/system/
 cp ./bind-api.conf /etc/
 cp ./bind-restapi.py /usr/local/bin/
 pip3 install -r requirements.txt
+systemctl enable bind-api.service
+systemctl start bind-api.service
